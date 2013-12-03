e44fb39df04266033853d829f25a622a42621714
David Wu <david@wu-man.com>
2013-12-04 02:04:00 +0800

    catch thread id parsing error

--- cmd/go-ck101.go
+++ cmd/go-ck101.go
@@ -31,7 +31,11 @@ func main() {
 		log.Fatalf("Failed to get current user: %v", err)
 	}
 	basedir := filepath.Join(u.HomeDir, "Pictures/go-ck101/")
-	threadId := regexp.MustCompile("thread-(\\d+)-.*").FindStringSubmatch(path.Base(url))[1]
+	matches := regexp.MustCompile("thread-(\\d+)-.*").FindStringSubmatch(path.Base(url))
+	if matches == nil || len(matches) < 2 {
+		log.Fatalf("URL should be in the form of http://ck101.com/thread-2593278-1-1.html")
+	}
+	threadId := matches[1]
 
 	b, err := ck101.GrabPage(url)
 	if err != nil {
